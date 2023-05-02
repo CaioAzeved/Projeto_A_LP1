0b5163b464d621bf1b3e53fed8101b158e853c62
Caio <caiocvam3@gmail.com>
2023-05-02 11:51:54 -0300

    Correção da busca_terminal2

--- clientes.cpp
+++ clientes.cpp
@@ -16,12 +16,17 @@ void listar2(std::map <std::string,Onibus> onibus, std::vector<std::string> nome
 //Função que busca um ônibus específico usando o terminal.
 void buscar_terminal2(std::map <std::string,Onibus> onibus, std::vector<std::string> nome){
     std::string term;
+    int fail = 0;
     std::cout << "Insira um terminal:" << std::endl;
-    std::cin >> term;
+    std::getline(std::cin >> std::ws, term);
     for(unsigned long long int i{0}; i < nome.size(); ++i){
         if(onibus[nome[i]].terminal == term){
             std::cout << nome[i] << std::endl;
+            fail = 1;
         }
+    }
+    if(fail != 1){
+        std::cout << "Terminal inválido." << std::endl;
     }
 }
 
@@ -53,8 +58,8 @@ void reclamar(){
 
 void menu_cli(std::map <std::string,Onibus> onibus, std::vector<std::string> nome){
     int num;
+    std::cout << "Digite um dos números para realizar uma das seguintes ações." << std::endl;
     while(num != 5){
-        std::cout << "Digite um dos números para realizar uma das seguintes ações." << std::endl;
         std::cout << "1-Listar ônibus cadastrados." << std::endl;
         std::cout << "2-Buscar ônibus." << std::endl;
         std::cout << "3-Mostrar itininerário." << std::endl;
@@ -62,19 +67,28 @@ void menu_cli(std::map <std::string,Onibus> onibus, std::vector<std::string> nom
         std::cout << "5-Encerrar." << std::endl;
         std::cin >> num;
         if(num == 1){
+            std::cout << std::endl;
             listar2(onibus, nome);
+            std::cout << std::endl;
         }
         else if(num == 2){
+            std::cout << std::endl;
             buscar_terminal2(onibus, nome);
+            std::cout << std::endl;
         }
         else if(num == 3){
+            std::cout << std::endl;
             ver(onibus);
+            std::cout << std::endl;
         }
         else if(num == 4){
+            std::cout << std::endl;
             reclamar();
+            std::cout << std::endl;
         }
         else{
             break;
         }
+        std::cout << "Deseja realizar mais alguma ação?" << std::endl;
     }
 }
